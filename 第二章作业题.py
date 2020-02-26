683d8a333447cb3f8e8472a7b136f9909cc24df0
dbefb <1160132795@qq.com>
2020-02-26 12:17:44 +0800

    慕课第二章作业题框架

---
+++ 第二章作业题.py
@@ -0,0 +1,30 @@
+import sys
+class txtHandle(object):
+    def __init__(path):
+        '''
+        导入待分析文件
+        '''
+        
+    def queryFile(path):
+        '''
+        导入查询文件，把里面的单词弄成一个列表1
+        '''
+
+    def fileAnalysis():
+        '''
+        对分析文件进行处理：1、拆成句子，弄成一个列表2，每个句子占列表一个元素，用下标进行查找
+                          2、对每一个列表1中的单词放到列表2中进行查询，查询到了以后记录当前列表2的下标，这就是第n+1个句子
+        '''
+    def print():
+        '''
+        输出所在位置
+        '''
+def main():
+    '''
+    sys.argv[1]为待分析文件
+    sys.argv[2]为查询文件
+    '''
+if __name__ == '__main__':
+    mian()
+    
+
