efa5512dec989c235515f620bdde8d8eab210b8a
dbefb <1160132795@qq.com>
2020-03-04 12:09:11 +0800

    计算器单元测试_seconde

--- test_caculator.py
+++ test_caculator.py
@@ -17,16 +17,16 @@ class TestCaculator(unittest.TestCase):
         self.assertEqual('ERROR', conversionFormula('1/(2+3))'))
         self.assertEqual('ERROR', conversionFormula('!&@'))
         self.assertEqual('ERROR', conversionFormula('*2'))
+        self.assertEqual
     def test_calculator(self):
         '''test method calculator'''
         self.assertEqual(27,calculator(['2','^','3','*','2','+','13','-','4','/','2']))
         self.assertEqual(3,calculator(['-','2','^','2','*','2','+','13','-','4','/','2']))
-        self.assertEqual(-1,calculator(['+','1']))
-
-        # self.assertEqual(512,calculator(['2','^','3','^','3']))
+        self.assertEqual(1,calculator(['+','1']))
+        self.assertEqual('ERROR',calculator(['*','2']))
     def test_remove_racket(self):
         '''test method remove_racket'''
-        self.assertEqual(7, remove_bracket(['-','(','2','-','3','*','(','1','+','2',')',')']))
+        self.assertEqual(['7.0'], remove_bracket(['-','(','2','-','3','*','(','1','+','2',')',')']))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
