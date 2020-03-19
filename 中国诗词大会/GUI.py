742739e92eb380f0216e24765d4d674fbe0e5ebf
dbefb <1160132795@qq.com>
2020-03-19 11:02:47 +0800

    GUI初步完善

--- 中国诗词大会/GUI.py
+++ 中国诗词大会/GUI.py
@@ -1,12 +1,3 @@
-
-#!/usr/bin/env python
-
-# -*- coding: utf-8 -*-
-
-# author:洪卫
-
- 
-
 import tkinter as tk  # 使用Tkinter前需要先导入
 import tkinter.messagebox
 import jieba
@@ -60,34 +51,41 @@ generator = WordCreate('D:\桌面\诗词库.txt')  #选择合适的路径
 
 window = tk.Tk()
 
-
 window.title('My Window')
 
- 
-window.geometry('400x100') 
+window.geometry('400x150') 
+buttons = []
+neirong = []
 
-# word,name,sentence = generator.random_choice()
-
-def des(word,name,sentence):
+def des(index,n):
     window = tk.Tk()
     window.title('My Window')
-    window.geometry('200x50')
-    word =tk.Label(window, text=word, bg='green', fg='white', font=('Arial', 12), width=30, height=2)
-    word.place(x=100,y=25,anchor='s')
-    
-    
+    window.geometry('500x150')
+    var=n[0]
+    word_display = tk.Label(window, text=var, bg='green', fg='white', font=('Arial', 12), width=60, height=2)
+    word_display.place(x=250,y=75,anchor='s')
+    answer_button = tk.Button(window,text='点击查看答案', font=('Arial', 12), width=10, height=1, command=lambda :word_display.config(text='来自《'+n[1]+'》的：'+n[2]))
+    answer_button.place(x=250,y=120,anchor='s')
 
-def hit_me():
+def create_key_word():
     b.destroy()
+    buttons = []
+    neirong = []
+    refresh_button = tk.Button(window, text='换一批', font=('Arial', 12), width=10, height=1, command=create_key_word)
+    refresh_button.place(x=200,y=140,anchor='s')
     for i in range(3):
         print(i)
         for j in range(4):
+            index = j+i*4
             word,name,sentence = generator.random_choice()
-            c = tk.Button(window, text=word, font=('Arial', 12), width=10, height=1, command=des(word,name,sentence))
+            sc_list = [word, name, sentence]
+            neirong.append(sc_list)
+            n = neirong[index]
+            c = tk.Button(window, text=word, font=('Arial', 12), width=10, height=1, command=lambda index=index,n=n:des(index,n))
             c.grid(row=i, column=j, padx=1, pady=1, ipadx=1, ipady=1)
+            buttons.append(c)
             print(word)
             
-
-b = tk.Button(window, text='hit me', font=('Arial', 12), width=10, height=1, command=hit_me)
-b.place(x=200, y=50, anchor='s')
+b = tk.Button(window, text='进入游戏', font=('Arial', 12), width=10, height=1, command=create_key_word)
+b.place(x=200, y=70, anchor='s')
 window.mainloop()
