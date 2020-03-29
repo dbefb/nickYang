228a3c7ed2f2f68287334caa255c4f9ac05514c5
dbefb <1160132795@qq.com>
2020-03-29 10:44:08 +0800

    GUI更新

--- 中国诗词大会/GUI.py
+++ 中国诗词大会/GUI.py
@@ -28,7 +28,7 @@ class WordCreate():
             content = [i for i in content if i != '']
             self.sc_list = []
             for i in content:
-                weak_list = re.split(r'[、。\n\s*]',i)
+                weak_list = re.split(r'[、。？：“”\n\s*]',i)
                 weak_list = [i for i in weak_list if i != '']
                 self.sc_list.append(weak_list)
         
@@ -51,7 +51,7 @@ generator = WordCreate('D:\桌面\诗词库.txt')  #选择合适的路径
 
 window = tk.Tk()
 
-window.title('My Window')
+window.title('出口成诗')
 
 window.geometry('400x150') 
 buttons = []
@@ -59,7 +59,7 @@ neirong = []
 
 def des(index,n):
     window = tk.Tk()
-    window.title('My Window')
+    window.title('出口成诗')
     window.geometry('500x150')
     var=n[0]
     word_display = tk.Label(window, text=var, bg='green', fg='white', font=('Arial', 12), width=60, height=2)
@@ -74,7 +74,7 @@ def create_key_word():
     refresh_button = tk.Button(window, text='换一批', font=('Arial', 12), width=10, height=1, command=create_key_word)
     refresh_button.place(x=200,y=140,anchor='s')
     for i in range(3):
-        print(i)
+        
         for j in range(4):
             index = j+i*4
             word,name,sentence = generator.random_choice()
@@ -84,7 +84,7 @@ def create_key_word():
             c = tk.Button(window, text=word, font=('Arial', 12), width=10, height=1, command=lambda index=index,n=n:des(index,n))
             c.grid(row=i, column=j, padx=1, pady=1, ipadx=1, ipady=1)
             buttons.append(c)
-            print(word)
+            
             
 b = tk.Button(window, text='进入游戏', font=('Arial', 12), width=10, height=1, command=create_key_word)
 b.place(x=200, y=70, anchor='s')
