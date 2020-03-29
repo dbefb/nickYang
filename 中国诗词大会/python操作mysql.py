202e8990182fc97460f2e1995d2d32514961b410
dbefb <1160132795@qq.com>
2020-03-29 10:42:59 +0800

    更新

--- 中国诗词大会/python操作mysql.py
+++ 中国诗词大会/python操作mysql.py
@@ -4,8 +4,10 @@ conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='newbegi
 
 cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
-cur.execute('SELECT * FROM poetry')
 
+cur.execute("INSERT INTO fucku(id,name,score)VALUES('%s','%s','%s')"%('4','李四','的'))
+
+conn.commit()        
 result = cur.fetchall()
 
-print(result[1]['name'])
+print(result)
