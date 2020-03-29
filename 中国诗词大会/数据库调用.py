2cf745e0f2e7eca35239219deddcba6ad25fbb38
dbefb <1160132795@qq.com>
2020-03-29 11:13:42 +0800

    数据库调用后进行分词

--- 中国诗词大会/数据库调用.py
+++ 中国诗词大会/数据库调用.py
@@ -1,16 +1,27 @@
 import pymysql
 import random
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='newbegin',db='lsj',charset='utf8')
+import jieba
+def CallMySql():
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='newbegin',db='lsj',charset='utf8')
 
-cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
-cur.execute('select * from poetry')
-poetry = cur.fetchall()
+    cur.execute('select * from poetry')
+    poetry = cur.fetchall()
 
-random_poetry = random.choice(poetry)
+    random_poetry = random.choice(poetry)
 
-random_poetry = {i:random_poetry[i] for i in random_poetry if random_poetry[i]!='NULL'}
+    random_poetry = {i:random_poetry[i] for i in random_poetry if random_poetry[i]!='NULL'}
 
-poetry_length = len(random_poetry)
+    poetry_length = len(random_poetry)
+    # print(random_poetry)
+    sentence = random_poetry['sentense_'+str(random.randint(1,poetry_length-2))]
+    seg_sentence = jieba.cut(sentence, cut_all=False)
+    seg_sentence = list(seg_sentence)
+    seg_sentence = [i for i in seg_sentence if i !='，']
+    key_word = random.choice(seg_sentence)
+    # print(key_word,sentence)
+    # print(random_poetry)
+    # print('word,name,sentence:',key_word,random_poetry['name'],sentence)
 
-print('{}中的一句话:{}'.format(random_poetry['name'],random_poetry['sentense_'+str(random.randint(1,poetry_length-2))]))
+    return key_word,random_poetry['name'],sentence
