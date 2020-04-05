302884f0328837cf6944c76ddacfc2898e428985
dbefb <1160132795@qq.com>
2020-04-05 18:41:05 +0800

    这周的操作更新

--- 中国诗词大会/爬取古诗词注释.py
+++ 中国诗词大会/爬取古诗词注释.py
@@ -1,6 +1,6 @@
 from urllib import  request,error
 from bs4 import BeautifulSoup
-
+import re
 if __name__ == '__main__':
 
     url = "https://so.gushiwen.org/gushi/xiaoxue.aspx"
@@ -17,8 +17,9 @@ if __name__ == '__main__':
         div = soup.find_all('a',{'target':'_blank'})
         div = div[1:-1]
         name = div[0].string
-        f = open('D:\桌面\诗词库.txt','w+',encoding='utf-8')
+        f = open('D:\桌面\诗词库-1-赏析.txt','w+',encoding='utf-8')
         num = 1
+        shang_xi = []
         for i in div:
         #     print(i.string)
         #     print(i.get('href'))
@@ -30,12 +31,31 @@ if __name__ == '__main__':
             
             soup=BeautifulSoup(html,'lxml')
             html=soup.prettify()
-            div2 = soup.find('meta',{'name':'description'})
-            print(div2.get('content'))
+            div2 = soup.findAll('div',{'class':'contyishang'})
+            # print(div2[0].text)
+            string = re.split(r'[注释]', div2[0].text)
+            num2 = 0
+            yiwen = []
+            for i in string:
+                if i == '':
+                    num2+=1
+                if num2 == 2:
+                    break
+                yiwen.append(i)
             
-            f.writelines([str(num),name,'\n',div2.get('content'),'\n'])
+            yiwen = ''.join(yiwen)
+            # print(yiwen)
+            yiwen = re.split(r'[。\n∨译文]', yiwen)
+            # print(yiwen)
+            yiwen = [i for i in yiwen if i != '']
+            yiwen = yiwen[1:]
+            yiwen = '。'.join(yiwen)
+            print(yiwen)
+            f.writelines([str(num),'\n',yiwen,'\n'])
             f.close
             num+=1
+            
+
     except error.URLError as e:
         print("URLError:{0}".format(e.reason))
         print("URLError:{0}".format(e))
